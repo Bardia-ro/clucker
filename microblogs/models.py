cc0e7ef2c304da26fbe12ac5992ba8d10f47a2af
Bardia Rokhzadifar <k2036646@kcl.ac.uk>
2021-10-17 01:02:37 +0100

    add test cases to test firstname lastname email

--- microblogs/models.py
+++ microblogs/models.py
@@ -1,6 +1,23 @@
+import re
+from django.core.validators import RegexValidator
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.lookups import Regex
 
 # Create your models here.
 class User(AbstractUser):
+    username = models.CharField(
+        blank=False,
+        max_length=30,
+        unique = True,
+        validators=[RegexValidator(
+            regex=r'^@\w{3,}$',
+            message='username must consist of @ followed by at least 3 alphanumerical'
+        )]
+
+    )
+
+    first_name = models.CharField(unique=False, max_length=50, blank=False)
+    last_name = models.CharField(unique=False, max_length=50, blank=False)
+    email = models.EmailField(max_length=50, unique=True, blank=False)
     bio = models.TextField()
--- microblogs/tests.py
+++ microblogs/tests.py
@@ -16,10 +16,28 @@ class UserModelTestCase(TestCase):
         )
         self._assert_user_is_valid()
 
-
-    def test_user_name_cannot_be_empty(self):
+    def test_user_name_cannot_be_blank(self):
         self.user.username = ''
         self._assert_user_is_invalid()
+
+    
+    def test_username_must_be_unique(self):
+        second_user = self._create_second_user()
+        self.user.username = second_user.username
+        self._assert_user_is_invalid()
+
+
+
+    def test_lastname_does_not_have_to_be_unique(self):
+        second_user = self._create_second_user()
+        self.user.last_name = second_user.last_name
+        self._assert_user_is_valid()
+
+    def test_firstname_does_not_have_to_be_unique(self):
+        second_user = self._create_second_user()
+        self.user.first_name = second_user.first_name
+        self._assert_user_is_valid()
+    
 
     def _assert_user_is_valid(self):
         try:
@@ -28,8 +46,69 @@ class UserModelTestCase(TestCase):
             self.fail('Test user should be valid')
 
     def _assert_user_is_invalid(self):
-        # we are trying to catch it using with.
         with self.assertRaises(ValidationError):
             self.user.full_clean()
+
+
+    def test_username_starts_with_at_sign(self):
+        self.user.username = 'bardia'
+        self._assert_user_is_invalid()
+
+
+
+    def test_username_must_have_alphanumerical_after_at(self):
+        self.user.username = '@bard!ia'
+        self._assert_user_is_invalid()
+
+
+    def test_username_must_have_have_atleast_3_chars(self):
+        self.user.username = '@br'
+        self._assert_user_is_invalid()
+
+
+    def test_username_must_have_only_one_at(self):
+        self.user.username = '@@bardia'
+        self._assert_user_is_invalid()
+
+
+    def test_first_name_cannot_be_blank(self):
+        self.user.first_name = ''
+        self._assert_user_is_invalid()
+
+
+    def test_last_name_cannot_be_blank(self):
+        self.user.last_name = ''
+        self._assert_user_is_invalid()
+
+
+    def test_first_name_not_more_than_50(self):
+        self.user.first_name = '' * 51
+        self._assert_user_is_invalid()
+
+    def test_email_must_not_have_more_than_one_at(self):
+        self.user.email = 'brokhzadifar@@gmail.com'
+        self._assert_user_is_invalid()
+    
+    
+    def test_email_contain_domain(self):
+        self.user.email = 'brokhzadifar@@gmail'
+        self._assert_user_is_invalid()
+    
+    def _create_second_user(self):
+        user = User.objects.create_user(
+            '@bardi',
+            first_name = 'bard',
+            last_name = 'rok',
+            email = 'brokhzadifr@gmail.com',
+            password = 'password123321',
+            bio = 'hello there!'
+        )
+        return user
+
+                    
+        
+  
+                    
+
 
         

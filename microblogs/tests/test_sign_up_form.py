b04c781b5b3a1302ac75bc79650139fd80dad16e
Bardia Rokhzadifar <k2036646@kcl.ac.uk>
2021-11-22 16:17:31 +0000

    more tests

--- microblogs/tests/test_sign_up_form.py
+++ microblogs/tests/test_sign_up_form.py
@@ -5,6 +5,8 @@ from django.forms.widgets import PasswordInput
 from django.test import TestCase
 from microblogs.models import User
 from microblogs.forms import SignUpForm
+from django.urls import reverse
+from django.contrib.auth.hashers import check_password
 
 class SignUpsFormTestCase(TestCase):
 
@@ -77,6 +79,22 @@ class SignUpsFormTestCase(TestCase):
         self.form_input['new_password'] = 'Wrongpassword123'
         form = SignUpForm(data=self.form_input)
         self.assertFalse(form.is_valid())
+
+    
+    def test_form_save_correctly(self):
+        form = SignUpForm(data=self.form_input)
+        before_count = User.objects.count()
+        form.save()
+        after_count = User.objects.count() 
+        self.assertEqual(after_count, before_count+1)
+        user = User.objects.get(username = '@janedoe')
+        self.assertEqual(user.first_name, 'Jane')
+        self.assertEqual(user.last_name, 'Doe')
+        self.assertEqual(user.email, 'janedoe@example.org')
+        self.assertEqual(user.bio, 'my bio')
+        is_password_correct = check_password('Password123', user.password)
+        self.assertTrue(is_password_correct)
+
 
 
     
--- microblogs/tests/test_sign_up_view.py
+++ microblogs/tests/test_sign_up_view.py
@@ -1,6 +1,8 @@
 from django.urls import reverse
 from django.test import TestCase
 from microblogs.forms import SignUpForm
+from microblogs.models import User
+from django.contrib.auth.hashers import check_password
 
 class SignUpViewTestCase(TestCase):
     pass
@@ -29,13 +31,35 @@ class SignUpViewTestCase(TestCase):
         self.assertFalse(form.is_bound)
 
     def test_unsseccessful_sign_up(self):
+        before_count = User.objects.count()
         self.form_input['username'] = 'BAD_username'
         response = self.client.post(self.url , self.form_input)
+        after_count = User.objects.count() 
+        self.assertEqual(after_count, before_count)
         self.assertEqual(response.status_code,200)
         self.assertTemplateUsed(response, 'sign_up.html')
         form = response.context['form']
         self.assertTrue(isinstance(form, SignUpForm))
         self.assertTrue(form.is_bound)
 
+
+    #follow should be set to true while asserting a redirect
+    def test_succesful_sign_up(self):
+        before_count = User.objects.count()
+        response = self.client.post(self.url , self.form_input, follow=True)
+        after_count = User.objects.count() 
+        self.assertEqual(after_count, before_count+1)
+        response_url = reverse('feed')
+        self.assertRedirects(response, response_url,status_code=302, target_status_code=200)
+        self.assertTemplateUsed(response, 'feed.html')
+        user = User.objects.get(username = '@janedoe')
+        self.assertEqual(user.first_name, 'Jane')
+        self.assertEqual(user.last_name, 'Doe')
+        self.assertEqual(user.email, 'janedoe@example.org')
+        self.assertEqual(user.bio, 'my bio')
+        is_password_correct = check_password('Password123', user.password)
+        self.assertTrue(is_password_correct)
+        
     
+
 
